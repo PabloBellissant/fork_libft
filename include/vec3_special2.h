7cb7262af37fd2dcfaee648d5c0352684367c683
pabellis <mail@bellissantpablo.fr>
2025-12-01 02:31:26 +0100

    added get_tangent and get_bitangent

--- include/vec3_special2.h
+++ include/vec3_special2.h
@@ -30,12 +30,20 @@ static inline t_vec3	vec3_normalize(const t_vec3 v)
 	return (vec3_div_scalar(v, len));
 }
 
-/*
-	Normalize without zero-check
-*/
-static inline t_vec3	unsafe_vec3_normalize(const t_vec3 v)
+static inline t_vec3	get_tangent(t_vec3 n)
 {
-	return (vec3_div_scalar(v, vec3_length(v)));
+	t_vec3	up;
+
+	if (fabsf(n.y) > 0.999f)
+		up = vec3(1, 0, 0);
+	else
+		up = vec3(0, 1, 0);
+	return (vec3_normalize(vec3_cross(up, n)));
+}
+
+static inline t_vec3 get_bitangent(t_vec3 n, t_vec3 tangent)
+{
+	return (vec3_cross(n, tangent));
 }
 
 static inline float	clamp_branchless(const float val, const float min,
